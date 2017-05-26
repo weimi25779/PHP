febc758bf45c2591ab5a2f3c17b14e447c796956
weimi <e3b236yd@yahoo.comyw>
2017-05-26 09:42:58 +0800

    0525v1

--- 0525_lab9.php
+++ 0525_lab9.php
@@ -3,9 +3,10 @@ $img=imagecreate(100,40);
 $img=imagecreatefromjpeg("./upload/poky.jpg");
 
 $blue=imagecolorallocate($img,0,0,255);
-imagettftext($img,24,0,0,100,$blue,
+
+imagettftext($img,24,315,10,100,$blue,
     "./fonts/fireflysung.ttf",
-    'Hello,World');
+    "Hello,World");
 
 header('Content-Type:image/jpeg');
 imagejpeg($img);
