7e7186d6a1b3bb25eb7bb340b85b2096aa29f90a
Snow <e3b236yd@yahoo.com.tw>
2017-06-02 23:51:01 +0800

    0602v4

--- 0602/0602_lab4.php
+++ 0602/0602_lab4.php
@@ -1,17 +1,17 @@
 <?php
     if (!isset($_GET['a'])){
-        echo 'F1';
-        die();
+        echo 'False 1';
+        die(); //這樣就不會繼續做下去
     }
     include "sqlWeimi.php";
     $pdo = new pdo($dsn, $user, $passwd, $opt);
     $account = $_GET['a'];
     $sql = "SELECT account FROM member WHERE account = ?";
-    $stmt = $pdo->prepare($sql);    // statement SQL
+    $stmt = $pdo->prepare($sql);
     $stmt->execute([$account]);
     if ($stmt->rowCount()>0) {
-        // account is exist
-        echo 'F2';
+        // account is exist,指拿取得到的account去資料庫找是否有相同的account,若找到表示該帳號已存在
+        echo 'False 2';
     }else{
         // OK
         echo 'OK';
--- 0602/0602_lab6.php
+++ 0602/0602_lab6.php
@@ -5,3 +5,13 @@
     $sql = "SELECT * FROM member";
     $stmt = $pdo->prepare($sql);    // statement(陳述) SQL用語
     $stmt->execute([$account]);
+
+    echo"<table>";
+    while ($obj = $stmt->fetchObject()){
+        echo '<tr>';
+        echo "<td>{$obj->id}</td>";
+        echo "<td>{$obj->account}</td>";
+        echo "<td>{$obj->passwd}</td>";
+        echo '</tr>';
+    }
+    echo"</table>";
